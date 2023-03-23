54d4204186dea8dd9098066a8ea9fd31cb81f9df
iwairikuto <wjrikuto@gmial.com>
2023-03-23 18:08:47 +0900

    追加

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-send-email',
+    author='Rikuto',
+    url='https://github.com/rikuto125/python-send-email.git',
     version='0.1',
     packages=find_packages(),
     install_requires=[
